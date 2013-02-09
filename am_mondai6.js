e78da21728778886425f39b02a7e05b30f98dbf8
今田 雄太郎 <yk@imada-no-MacBook-Air.local>
2013-02-09 23:27:55 +0900

    update am_mondai6.js

--- am_mondai6.js
+++ am_mondai6.js
@@ -1,4 +1,27 @@
 var am_mondai6 = function(am)
 {
-    console.log("test");
+    var que = [am.stateInit];
+    var checklist = [];
+    if (statesFin.length==0)
+	return true;
+    
+    while (que.length > 0){
+	var state = que.shift();
+	checkList[state] = true;
+	for (var i=0; i<am.statesFin.length; i++)
+	    if (am.statesFin[i]==state)
+		return false;
+
+	for (var i=0; i<am.alphabets.length; j++){
+	    var nxtstate = am.transFunc[state][am.alphabets[i]];
+	    
+	    for (var i=0; i<am.statesFin.length; i++)
+		if (am.statesFin[i]==nxtstate)
+		    return false;
+	    
+	    if (nxtstate!==undefined)
+		que.push(nxtstate);
+	}
+    }
+    return true;
 }
